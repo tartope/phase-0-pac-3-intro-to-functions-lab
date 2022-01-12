e8cbb26074cc6e6032ddc53ac18586446e293bc0
Tunisia Artope <artope@yahoo.com>
2022-01-12 17:15:23 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+function whisper(string) {
+    return string.toLowerCase();
+}
+function logShout(string) {
+    console.log(string.toUpperCase());
+}
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+}
+function sayHiToHeadphonedRoommate(string) {
+    if (whisper="hello") {string = `I can\'t hear you!`}
+    else if (shout="HELLO") {string = `YES INDEED`}
+    // else if (string="Let's have dinner together!") {greeting = `I would love to`}
+
+}
